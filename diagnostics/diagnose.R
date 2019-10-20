13e86bc776a77ea69cac1e0979978d3f6e6952e3
Mathias <mathias.w.engstrom@gmail.com>
2019-10-20 21:58:05 +0200

    not quite there..

---
+++ diagnostics/diagnose.R
@@ -0,0 +1,143 @@
+diagnose <- function(network, cases) {
+  
+  
+  ################## Calculate probabilities for samples ###################
+  
+  P <- function(sample, known, probs = network) {
+    
+    P_pn = probs$P_pn
+    P_tb = probs$P_tb
+    P_lc = probs$P_lc
+    P_br = probs$P_br
+    P_te = probs$P_te
+    P_vtb = probs$P_vtb
+    P_sm = probs$P_sm
+    P_xr = probs$P_xr 
+    P_dy = probs$P_dy
+    
+    # Probabilities are found on index value + 1 for given attribute 
+    
+    # Unknown 
+    pn = sample$pn + 1
+    tb = sample$tb + 1
+    lc = sample$lc + 1
+    br = sample$br + 1
+    
+    # Known
+    te = known$te + 1
+    vtb = known$vtb + 1
+    sm = known$sm + 1
+    xr = known$xr + 1
+    dy = known$dy + 1
+    
+    probability <- P_pn[pn] * P_tb[tb] * P_lc[lc, sm] * P_br[br, sm] *
+      P_te(te)[pn] * P_vtb[vtb] * P_sm[sm] * P_xr[xr, pn, tb, lc] *
+      P_dy[dy, lc, br]
+
+    return(probability)
+  }
+  
+  #########3 Candidate function for binary attributes #####################
+  
+  f <- function(X) {
+    return(abs(X-1))
+  }
+  
+  ######################### MCMC Metropolitan in Gibbs ###########################
+  
+  # Task is to determine: 
+  # P(Pn | Te, VTB, Sm, XR, Dy)
+  # P(TB | Te, VTB, Sm, XR, Dy)
+  # P(LC | Te, VTB, Sm, XR, Dy)
+  # P(Br | Te, VTB, Sm, XR, Dy)
+  
+  # E = {Te, VTB, Sm, XR, Dy}
+  # U = {Pn, TB, LC, Br}
+  # f_Pn = f_TB = f_LC = f_Br: f(X) = 0 if X = 1, f(X) = 1 otherwise
+  
+  metro_in_gibbs <- function(te, vtb, sm, xr, dy) {
+    
+    sample_size <- 1000
+    burn_size <- 0.1*sample_size
+    zeros <- numeric(sample_size)
+    
+    known <- list(te = te, vtb = vtb, sm = sm, xr = xr, dy = dy)
+    samples <- data.frame(pn = zeros, tb = zeros, lc = zeros, br = zeros)
+    
+    random_values <- runif(4)
+    pn <- as.integer(random_values[1] > 0.5)
+    tb <- as.integer(random_values[2] > 0.5)
+    lc <- as.integer(random_values[3] > 0.5)
+    br <- as.integer(random_values[4] > 0.5)
+    samples[1, ] <- c(pn, tb, lc, br)
+    
+    n_unknown <- dim(samples)[2]
+    
+    # For each sample i
+    for(i in 2:sample_size){
+      
+      # Assigned is equal to previous sample 
+      assigned <- samples[i-1, ]
+      
+      # For each unknown parameter p
+      for(p in 1:n_unknown) {
+        
+        # Proposed is equal to assigned, with unknown parameter p replaced by
+        # f(p)
+        
+        proposed <- assigned
+        proposed[p] <- f(proposed[p])
+
+        # Calculate p for new and old
+        P_old <- P(assigned, known)
+        P_new <- P(proposed, known)
+
+        if(P_new > P_old) {
+          assigned <- proposed
+          
+        } else {
+          random <- runif(1)
+          ratio <- P_new/P_old
+
+          if(random < ratio) {
+            assigned <- proposed
+          }
+          
+        }
+        
+      }
+      samples[i, ] <- assigned
+      
+      
+    }
+
+    samples <- tail(samples, -burn_size)
+    n_samples = dim(samples)[1]
+    P_pn_1 <- length(which(samples$pn == 1))/n_samples
+    P_tb_1 <- length(which(samples$tb == 1))/n_samples
+    P_lc_1 <- length(which(samples$lc == 1))/n_samples
+    P_br_1 <- length(which(samples$br == 1))/n_samples
+    
+    P_out = c(P_pn_1, P_tb_1, P_lc_1, P_br_1)
+    #print(P_out)
+    return(P_out)
+  }
+
+  output <- matrix(0, 10, 4)
+  
+  for(i in 1:dim(cases)[1]) {
+    case <- cases[i, ]
+    te <- case$Te
+    vtb <- case$VTB
+    sm <- case$Sm
+    xr <- case$XR
+    dy <- case$Dy
+    
+    
+    output[i,] <- metro_in_gibbs(te, vtb, sm, xr, dy)
+    
+  }
+  print(output)
+  return(output)
+  
+}
---
+++ diagnostics/learn.R
@@ -0,0 +1,151 @@
+learn <- function(hist) {
+  
+  pn <- hist$Pn
+  te <- hist$Te
+  vtb <- hist$VTB
+  tb <- hist$TB
+  sm <- hist$Sm
+  lc <- hist$LC
+  br <- hist$Br
+  xr <- hist$XR
+  dy <- hist$Dy
+  
+  
+  # Number of training examples
+  N_train <- dim(hist)[1]
+  
+  # P(Pn = 1) , P(Pn = 0)
+  P_pn <- matrix(0, 1, 2)
+  n_pn_0 <- length(which(pn == 0)) + 1
+  P_pn[1] <- n_pn_0/(N_train + 2)  # Pn = 0
+  P_pn[2] <- 1 - P_pn[1]     # Pn = 1
+  
+  
+  # P(VTB= 1) , P(VTB = 0) 
+  P_vtb <- matrix(0, 1, 2)
+  n_vtb_0 <- length(which(vtb == 0)) + 1
+  P_vtb[1] <- n_vtb_0/(N_train + 2)
+  P_vtb[2] <- 1 - P_vtb[1]
+  
+  # P(Sm = 1) , P(Sm = 0)
+  P_sm <- matrix(0, 1, 2)
+  n_sm_1 <- length(which(sm == 0)) + 1
+  P_sm[1] <- n_sm_1/(N_train + 2)
+  P_sm[2] <- 1 - P_sm[1]
+  
+  # P(Te = X| Pn = 1) , P(Te = X| Pn = 0) 
+  P_te <- function(temp, te. = te) {
+
+    # Pn = 1
+    pn_1_idx <- which(pn == 1)
+    te_pn_1_mean <- mean(te.[pn_1_idx])
+    te_pn_1_std <- sd(te[pn_1_idx])
+    D_te_pn_1 <- dnorm(temp, te_pn_1_mean, te_pn_1_std)
+  
+    # Pn = 0
+    pn_0_idx <- which(pn == 0)
+    te_pn_0_mean <- mean(te.[pn_0_idx])
+    te_pn_0_std <- sd(te[pn_0_idx])
+    D_te_pn_0 <- dnorm(temp, te_pn_0_mean, te_pn_0_std)
+    
+    # Convert densities to probabilities (?)
+    P_te_pn_0 <- D_te_pn_0/(D_te_pn_1 + D_te_pn_0)
+    P_te_pn_1 <- D_te_pn_1/(D_te_pn_1 + D_te_pn_0)
+    
+    
+    return(c(P_te_pn_0, P_te_pn_1))  
+  }
+  
+  
+  # P(TB = 1 | VTB = 1) , P(TB =  1| VTB= 0) and 
+  # P(TB = 0 | VTB = 1) , P(TB = 0 | VTB = 0)
+  
+  P_tb <- matrix(0, 2, 2)
+  
+  for(i in 1:2) {
+    for(j in 1:2){
+      tb_idx <- which(tb == i-1)
+      vtb_idx <- which(vtb == j-1)
+      
+      P_tb[i, j] <- (length(intersect(tb_idx, vtb_idx)) + 1)/(length(vtb_idx) + 2)
+    }
+  }
+  
+  
+  # P(LC = 1 | Sm = 1) , P(LC =  1| Sm = 0) and 
+  # P(LC = 0 | Sm = 1) , P(LC = 0 | Sm = 0)
+  
+  P_lc <- matrix(0, 2, 2)
+  
+  for(i in 1:2) {
+    for(j in 1:2){
+      lc_idx <- which(lc == i-1)
+      sm_idx <- which(sm == j-1)
+      
+      P_lc[i, j] <- (length(intersect(lc_idx, sm_idx)) + 1)/(length(sm_idx) + 2)
+    }
+  }
+  
+  # P(Br = 1 | Sm = 1) , P(Br =  1| Sm = 0) and 
+  # P(Br = 0 | Sm = 1) , P(Br = 0 | Sm = 0)
+  
+  P_br <- matrix(0, 2, 2)
+  
+  for(i in 1:2) {
+    for(j in 1:2){
+      br_idx <- which(br == i-1)
+      sm_idx <- which(sm == j-1)
+      
+      P_br[i, j] <- (length(intersect(br_idx, sm_idx)) + 1)/(length(sm_idx) + 2)
+    }
+  }
+  
+  # P(XR| Pn, TB, LC)
+  
+  P_xr <- array(0, c(2,2,2,2))
+  
+  for(i in 1:2) {
+    for(j in 1:2){
+      for(k in 1:2){
+        for(l in 1:2){
+          xr_idx <- which(xr == i-1) # index 1 in array
+          pn_idx <- which(pn == j-1) # index 2 in array
+          tb_idx <- which(tb == k-1) # index 3 in array
+          lc_idx <- which(lc == l-1) # index 4 in array
+        
+          intersection <- intersect(pn_idx, intersect(tb_idx, lc_idx))
+          
+          P_xr[i, j, k, l] <- (length(intersect(xr_idx, intersection)) + 1)/(length(intersection) + 2)
+          
+        }
+      }
+    }
+  }
+
+  # P(Dy| LC, Br)
+  
+  P_dy <- array(0, c(2,2,2))
+  
+  for(i in 1:2) {
+    for(j in 1:2){
+      for(k in 1:2){
+  
+          dy_idx <- which(dy == i-1)
+          lc_idx <- which(lc == j-1)
+          br_idx <- which(br == k-1)
+
+          
+          intersection <- intersect(lc_idx, br_idx)
+
+          P_dy[i, j, k] <- (length(intersect(dy_idx, intersection)) + 1)/(length(intersection) + 2)
+          }
+    }
+  }
+  
+  output <- list(P_pn = P_pn, P_tb = P_tb, P_lc = P_lc, P_br = P_br,
+                 P_te = P_te, P_vtb = P_vtb, P_sm = P_sm, 
+                 P_xr = P_xr, P_dy = P_dy)
+  
+  
+  return(output)
+}
